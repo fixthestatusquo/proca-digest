b48f55c51bad53fd85e17b4cd241cab52213c4ef
1v4n4 <ivananl@yahoo.com>
2023-03-29 20:00:41 +0200

    Add errors if template doesn't exist

--- template.js
+++ template.js
@@ -5,15 +5,23 @@ require("dotenv").config();
 const from = process.env.REACT_APP_TEMPLATES_FOLDER;
 
 const subject = (campaign, name, lang) => {
-  const p = path.resolve(__dirname, from +`${campaign}/${name}/${lang}.json`);
+  const p = path.resolve(__dirname, from + `${campaign}/${name}/${lang}.json`);
+  if (!fs.existsSync(p)) {
+    console.error("Subject does not exist:", p);
+    return;
+  }
   return JSON.parse(fs.readFileSync(p, "utf8")).meta.subject;
 };
 
 const html = (campaign, name, lang) => {
   const p = path.resolve(__dirname, from + `${campaign}/${name}/${lang}.html`);
+  if (!fs.existsSync(p)) {
+    console.error("HTML does not exist:", p);
+    return;
+  }
   return fs.readFileSync(p, "utf8");
 };
 
-console.log(subject("restorenaturepics", "initialDigest", "en"), html("restorenaturepics", "initialDigest", "en"));
+// insert variables in templates code here
 
 module.exports = { subject, html };
