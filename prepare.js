58568a2cc69b6d1ca07cc8e153e463f99a062b6c
=Xavier <github@tttp.eu>
2023-04-14 12:52:16 +0200

    WIP

--- prepare.js
+++ prepare.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 const { subject, html, insertVariables } = require("./template");
 const { supabase } = require("./api");
 const getTargets = require("./targets").getTargets;
+const color = require("cli-color");
 const argv = require("minimist")(process.argv.slice(2), {
-const argv = require("minimist")(process.argv.slice(2), {
-  boolean: ["help","dry-run"],
-  default: {"template":"default"}
+  boolean: ["help", "dry-run"],
+  default: { template: "default" },
 });
-
 
 const help = () => {
   console.log(
@@ -22,77 +21,86 @@ const help = () => {
   process.exit(0);
 };
 
+const createdAt = new Date();
+const  dateFormat = date => {
+  const utc = 'getUTC';// 'get'?
+  return "%Y-%m-%d %H:%M:%S".replace (/%[YmdHMS]/g, function (m) {
+    switch (m) {
+    case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
+    case '%m': m = 1 + date[utc + 'Month'] (); break;
+    case '%d': m = date[utc + 'Date'] (); break;
+    case '%H': m = date[utc + 'Hours'] (); break;
+    case '%M': m = date[utc + 'Minutes'] (); break;
+    case '%S': m = date[utc + 'Seconds'] (); break;
+    default: return m.slice (1); // unknown code, remove %
+    }
+    // add leading zero if required
+    return ('0' + m).slice (-2);
+  }) + "+00";
+}
+
 const campaign = argv._[0];
 
 if (argv.help || !campaign) return help();
-
 if (!argv.source) argv.source = campaign;
 let templateName = argv["template"] || "default"; // TODO: for each target, check if the target has received an email, "initial", otherwise, "default"
 const sourceName = argv["source"];
 
 const targets = getTargets(sourceName);
 console.log("targetting ", targets.length, " from ", sourceName);
-// it is set to send emails with gmail
-// usual pasword not working, use app password https://support.google.com/accounts/answer/18583
+console.log(
+  color.green("timestamp of the digest", createdAt, dateFormat(createdAt))
+);
 
+const prepare = async (target, templateName, campaign) => {
+  if (!target.lang && argv.lang) {
+    console.warn("no language for ", target.email);
+  }
+  const lang = target.lang || argv.lang;
+  const s = subject(campaign, templateName, target.lang);
+  const h = html(campaign, templateName, target.lang);
+  if (!s) {
+    console.error("Subject not found:", target);
+    throw new Error("Subject not found:", target);
+  }
+  if (!h) {
+    console.error("HTML not found:", target);
+    throw new Error("HTML not found:", target);
+  }
+  // fetch variables
+  // insert variables in template
+  insertVariables(h, (variables = ""));
+  console.log("lang", lang);
 
-  // Preview only available when sending through an Ethereal account!!
-  //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  const { data, error } = await supabase.from("digest").insert([
+    {
+      created_at: createdAt,
+      subject: s,
+      body: h,
+      status: "pending",
+      template: templateName,
+      email: target.email,
+      target_id: target.externalId || target.email,
+      variables: {},
+    }, // , template: `${campaign}/${name}` },
+  ]);
 
-  // add lang to template record?
-  //we dont have target_id on source, use email instead???
-/*
-    "externalId": "recJ8DP6xzKIMaIJd",
-    "name": "Achille VARIATI",
-    "email": "achille.variati@europarl.eu",
-    "area": "IT",
-    "field": {
-      "epid": 239257,
-      "gender": "M",
-      "eugroup": "S&D",
-      "party": "Partito Democratico",
-      "sort": "variati achille"
-    }
-*/
-
-const prepare = async (target, templateName,campaign) => {
-    if (!target.lang && argv.lang) {
-      console.warn("no language for ",target.email);
-    }
-    const lang=target.lang|| argv.lang;
-    const s = subject(campaign, templateName, target.lang)
-    const h = html(campaign, templateName, target.lang)
-    if (!s) {
-      console.error("Subject not found:", target);
-      throw new Error("Subject not found:", target);
-    }
-    if (!h) {
-      console.error("HTML not found:", target);
-      throw new Error("HTML not found:", target);
-    }
-      // fetch variables
-      // insert variables in template
-    insertVariables(h, variables = "");
-
-  const { data, error } = await supabase
-  .from('digest')
-  .insert([
-    { subject: s, body: h, status: "pending", template: templateName, email: target.email, target_id: target.externalId||target.email, variables: {} }// , template: `${campaign}/${name}` },
-  ])
-
-  if (error) console.log('error saving template', error)
-}
+  if (error) {
+    console.log("error saving template", error);
+    return error;
+  }
+  console.log(data);
+};
 
 const main = async () => {
   for (const i in targets) {
     const target = targets[i];
     // todo: if template not set, supabase.select email,target_id from digests where campaign=campaign and status='sent' group by email
     // if in that list -> template= default, else -> initial
-    prepare (targets[i], templateName);
+    await prepare(targets[i], templateName, campaign);
 
-process.exit(1);
-    }
+    process.exit(1);
   }
-}
+};
 
 main().catch(console.error);
